d033b393c8c20710b4b74974084a61f072b5ef4e
Vinicius Negrao de Menezes <vnsn_@outlook.com>
2019-11-16 20:01:05 -0300

    Testing getting params from url

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.get('/:name', (req, res) => {
+    console.log(req);
+    res.send(`Hello ${req.params.name}`)
+})
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Node API working!!!');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
