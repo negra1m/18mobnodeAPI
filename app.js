c21e42349d3efb88353fbf5d79c0a5dff00e8a81
Vinicius Negrao de Menezes <vnsn_@outlook.com>
2019-11-24 15:03:43 -0300

    Setting App

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
+/**
+ * Define main application imports
+ */
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/:name', (req, res) => {
-    console.log(req);
-    res.send(`Hello ${req.params.name}`)
-})
+/**
+ * Main router config
+ */
+const routes = require('./src/routes/app')
+app.use(routes)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-var firebase = require("firebase-admin");
-
-var serviceAccount = require("./marmitapp-firebase-config.json");
-
-firebase.initializeApp({
-    credential: firebase.credential.cert(serviceAccount),
-    databaseURL: "https://marmitapp-8233a.firebaseio.com"
-});
