9dc465a227c09af38d74e5700f85aeabda2f5b2a
Vinicius Negrao de Menezes <vnsn_@outlook.com>
2019-11-24 18:08:41 -0300

    fix for heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 8080
+const port = process.env.PORT || 3000;
 
 /**
  * Main router config
